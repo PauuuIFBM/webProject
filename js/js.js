b64e3356425f4bc3d6ab59b415233203f6bd5476
Pau <43619813+PauuuIFBM@users.noreply.github.com>
2018-12-13 19:14:16 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+   
+
+    $("#accordion").accordion({
+        
+    });
+  
+
+});
+
+$(function() {
+    var options = {
+        modules: {
+            toolbar: {
+                toolbar: toolbarOptions
+            }
+        },
+        placeholder: 'Escribeme o comparte tus fotos, fuentes o cualquier cosa conmigo :D',
+        readOnly: false,
+        theme: 'snow'
+    }
+
+    var container = $('#editor').get(0);
+    var toolbarOptions = [['bold', 'italic'], ['link']];
+
+    var quill = new Quill(container, options);
+});
+
+
+
+
+
